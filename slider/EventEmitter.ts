f7e3f55893e7e832c4cc7bcf4a5445be7513ef31
Axepuff <axepuff@gmail.com>
2019-01-30 02:04:58 +0300

    feat: EventEmitter init

---
+++ slider/EventEmitter.ts
@@ -0,0 +1,29 @@
+class EventEmitter {
+  private events: object;
+
+  constructor() {
+    // this.events = {};
+  }
+  
+  public trigger<T>(eventName: string, data: T): void {
+    const event = this.events[eventName];
+    if (event) {
+      event.forEach(fn => {
+        fn.call(null, data);
+      });
+    }
+  }
+  
+  public on(eventName: string, fn: void) {
+    if(!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    
+    this.events[eventName].push(fn);
+    return () => {
+      this.events[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn);
+    }
+  }
+  
+ 
+}
